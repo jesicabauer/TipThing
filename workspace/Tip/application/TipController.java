3432f14aec021b2c192abdfd98fa244f739f0f79
Ben Matthews <bmatthews@carroll.edu>
2016-04-26 09:44:51 -0600

    Added files

--- workspace/Tip/application/TipController.java
+++ workspace/Tip/application/TipController.java
@@ -1,6 +1,9 @@
 import java.awt.TextField;
+import java.math.RoundingMode;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
 package application;
 
 
@@ -11,6 +14,10 @@ private TextField amountTextField;
 	@FXML
 private TextField totalTextField;
 	@FXML
+private Slider TipSlider;
+	@FXML
+
+
 private void calculateButtonPressed(ActionEvent event){
 		Float mealAmount = Float.parseFloat(amountTextField.getText());
 		totalTextField.setText(String.valueOf(mealAmount * (1 + .20)));
