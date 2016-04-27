e085b652415de3196e74ee0a5b68606ecc8dbe88
Jesica Bauer <Jesica Bauer>
2016-04-27 11:23:23 -0600

    Added controller information

--- workspace/Tip/application/TipController.java
+++ workspace/Tip/application/TipController.java
@@ -1,25 +1,67 @@
 import java.awt.TextField;
+import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.NumberFormat;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 package application;
 
 
 
 public class TipController {
+	private final static NumberFormat currency = NumberFormat.getCurrencyInstance(); 
+	private final static NumberFormat percent = NumberFormat.getPercentInstance(); 
+	
+	private BigDecimal tipPercentage = new BigDecimal(0.15);
 	@FXML
 private TextField amountTextField;
+	@FXML 
+private Label tipPercentageLabel;
+	@FXML 
+private TextField tipTextField; 
 	@FXML
 private TextField totalTextField;
 	@FXML
 private Slider TipSlider;
 	@FXML
-
-
+	
+	@FXML
 private void calculateButtonPressed(ActionEvent event){
-		Float mealAmount = Float.parseFloat(amountTextField.getText());
-		totalTextField.setText(String.valueOf(mealAmount * (1 + .20)));
+		figureTotal(); 
 	}
+	
+	public void initialize() {
+		currency.setRoundingMode(RoundingMode.HALF_UP);
+		
+		TipSlider.valueProperty().addListener (
+				new ChangeListener<Number>(){
+					@Override
+					public void changed(ObservableValue<? extends Number> ov,
+						Number oldValue, Number newValue) {
+						tipPercentage =
+								BigDecimal.valueOf(newValue.intValue()/100.0);
+						tipPercentageLabel.setText(percent.format(tipPercentage));
+						figureTotal();
+					}
+				} );
+	}
+	private void figureTotal() {
+		try {
+			BigDecimal amount = new BigDecimal(amountTextField.getText());
+			BigDecimal tip = amount.multiply(tipPercentage);
+			BigDecimal total = amount.add(tip);
+			
+			tipTextField.setText(currency.format(tip));
+			totalTextField.setText(currency.format(total));
+		} catch (NumberFormatException ex) {
+			amountTextField.setText("Enter Amount, please");
+			amountTextField.selectAll(); 
+			amountTextField.requestFocus(); 
+		}
+		}
+	}
+	
 }
